918b509f1277eed1289d18d5be6a15c67c99e7d2
Christopher868 <Christopherhicks868@gmail.com>
2025-04-01 21:36:47 -0400

    Added Dropdown account menu to navbar

--- static/js/base.js
+++ static/js/base.js
@@ -6,3 +6,33 @@ setTimeout(function () {
         setTimeout(() => alert.remove(), 500);
     });
 }, 3000);  // Message disappears after 3 seconds
+
+
+
+// function toggleDisplay(elementId) {
+//     const element = document.getElementById(elementId);
+
+//     console.log(element);
+//     if (element.style.display === "none") {
+//         element.style.display = "block";
+//     } else {
+//         element.style.display = "none";
+//     }
+// }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let menu = document.getElementById("dropdown-list-id");
+    let account_button = document.getElementById("account-dropdown-button");
+    let menu_button = document.getElementById("menu-dropdown-button");
+
+    account_button.addEventListener("click", function () {
+        let currentDisplay = window.getComputedStyle(menu).display;
+        menu.style.display = (currentDisplay === "none") ? "block" : "none";
+    });
+
+    menu_button.addEventListener("click", function () {
+        let currentDisplay = window.getComputedStyle(menu).display;
+        menu.style.display = (currentDisplay === "none") ? "block" : "none";
+    });
+});
